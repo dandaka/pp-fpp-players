20dfbd75400bd3637a83438257258ca5f11c5c2b
Vlad Ra <vlad19@gmail.com>
2026-03-25 11:48:45 +0000

    feat(db): update tournament queries for category_code and tournament_players

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,3 +3,4 @@ export * from "./types";
 export { searchPlayers, getTopPlayers, getPlayer, getPlayerRanks, getPlayerRating, getPlayerTournamentsCount, getPlayerMatchesCount, getPlayerStartYear } from "./queries/players";
 export { getPlayerMatches, getPlayerUpcomingMatches } from "./queries/matches";
 export { getTournaments, getTournament, getTournamentCategories, getTournamentPlayers, getTournamentMatches } from "./queries/tournaments";
+export type { CategoryInfo } from "./queries/tournaments";
